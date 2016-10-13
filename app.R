75ec4dd840fa09657f2749dd2dd47e98c82949df
Martin Aryee <martin.aryee@gmail.com>
2016-10-12 23:20:57 -0400

    Added base dir size history

--- app.R
+++ app.R
@@ -22,11 +22,10 @@ ui <- dashboardPage(
         h3(textOutput("timestamp")),
         fluidRow(
           box(title="Biggest directories", plotOutput("bigDirs", width=700, height = 600))
+        ),
+        fluidRow(
+          box(title="Directory size history", plotOutput("history", width=500, height = 250))
         )
-        #,
-        #fluidRow(
-        #  box(title="Directory size history", plotOutput("history", width=500, height = 250))
-        #)
       ),
       tabItem(tabName = "cid",
               fluidRow(
@@ -66,53 +65,9 @@ server <- function(input, output) {
   
   # Directory size history
   ## Get directories to plot (Current biggest top-level dirs)
-  x <- subset(dat, dat$date==max(dat$date))
-  x <- x[order(x$sizeKB, decreasing = TRUE),]
-  # Get top-level dir
-  level <- str_count(x$dir, "/")
-  baseDir <- x[which(level == min(level)), "dir"]
-  # Drop top level prefix
-  baseDir
-  x$dir <- sub(paste0(baseDir, "/"), "", x$dir)
-  dat$dir <- sub(paste0(baseDir, "/"), "", dat$dir)
-  # Get level 1 dirs
-  xx <- x[which(level == min(level)+1), ]
-  xx <- subset(xx, sizeTB>=1)
-  otherKB <- x[x$dir==baseDir, "sizeKB"] - sum(xx$sizeKB)
-  otherTB <- otherKB/1e9
-  topDirs <- c(xx$dir[1:min(10, nrow(xx))], "Other")
-  topTB <- c(xx$sizeTB, otherTB)
-  
-  # Get sizes of top-level dirs and 'other' for all datetimes
-  topDat <- ddply(dat, .(date), function(x) {
-    rownames(x) <- x$dir
-    xx <- subset(x, dir %in% topDirs)
-    head(xx)
-    otherKB <- x[baseDir,"sizeKB"] - sum(xx$sizeKB)
-    otherTB <- otherKB/1e9
-    xx <- rbind(xx, 
-                data.frame(date=xx$date[1], dir="Other", sizeKB=otherKB, sizeTB=otherTB, stringsAsFactors = FALSE)
-    )
-    xx
-  })
-  
-  if (FALSE) {
-    # Replace NAs with zeros. (Also converts dates to factors unfortunately)
-    m <- melt(topDat, id.vars = c("date", "dir"), measure.vars = "sizeTB")
-    w <- acast(m, dir ~ date)
-    w[is.na(w)] <- 0
-    topDat <- melt(w,varnames = c("dir", "date"), value.name = "sizeTB")
-  }
-  
-  topDat$dir <- factor(topDat$dir, levels=topDirs)
-  xmin <- min(topDat$date)
-  xmax <- min(topDat$date) + 1.2 * (max(topDat$date) - min(topDat$date))
-  #p <- ggplot(topDat, aes(date, sizeTB, fill=dir, group=dir)) + geom_area() +  aes(order = as.numeric(-sizeTB)) + xlim(xmin, xmax) + theme_bw()
-  p <- ggplot(topDat, aes(date, sizeTB, fill=dir, group=dir)) + geom_area() + xlim(xmin, xmax) + theme_bw()
-  cs <- cumsum(topTB)
-  y <- rowMeans(cbind(c(0, cs[-length(cs)]), cs))
-  pHistory <- p + annotate("text", x = max(topDat$date), y=y, label=topDirs, hjust=-0.1) + guides(fill=FALSE)
-  
+  x <- subset(dat, dir==baseDir)
+  pHistory <- ggplot(x, aes(date, sizeTB)) + geom_area(fill="blue") + theme_bw()
+
   output$heading <- renderText({paste0(baseDir, "  (", baseDirTB, "TB)")})
   
   output$timestamp <- renderText({paste(as.character(max(dat$date)), "UTC")})
